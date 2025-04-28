0d83297160209ae10c804e54a39e68798012145a
Navreet Singh <naviibrar011@gmail.com>
2025-04-28 20:11:03 +0530

    Add Topbar with Timer display
    Drew an aqua-colored top bar at the top of the window and added a timer label displaying elapsed time.
    The timer is rendered using LABEL_FONT and formatted with the format_time function.
    Positioned the timer label at (5, 5) on the window.

--- main.py
+++ main.py
@@ -69,6 +69,13 @@ def format_time(secs):
 
 
 def draw_top_bar(win, elapsed_time, targets_pressed, misses):
+    pygame.draw.rect(win, "aqua", (0, 0, WIDTH, TOP_BAR_HEIGHT ))
+    time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, "black")
+
+
+    win.blit(time_label, (5, 5))
+
+
 
 def main():
     run = True
