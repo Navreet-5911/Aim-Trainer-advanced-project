85b07f62e33ac3b869a09403911f462dcf453a7d
Navreet Singh <naviibrar011@gmail.com>
2025-04-29 22:07:13 +0530

    fix:calculate and display target hit speed
    Fix target hit speed calculation and display
    
    - Added real-time calculation of hit speed (targets per second)
    - Displayed speed in the top bar using rounded value
    - Ensures accurate performance feedback for the player

--- main.py
+++ main.py
@@ -72,6 +72,8 @@ def draw_top_bar(win, elapsed_time, targets_pressed, misses):
     pygame.draw.rect(win, "aqua", (0, 0, WIDTH, TOP_BAR_HEIGHT ))
     time_label = LABEL_FONT.render(f"Time: {format_time(elapsed_time)}", 1, "black")
 
+    speed = round(targets_pressed / elapsed_time, 1)
+    speed_label  = LABEL_FONT.render(f"Speed: {speed} t/s", 1, "black")
 
     win.blit(time_label, (5, 5))
 
