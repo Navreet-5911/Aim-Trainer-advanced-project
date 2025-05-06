548828b673aa66b9a5a12ae316eaabbefd83868e
Navreet Singh <naviibrar011@gmail.com>
2025-05-06 20:36:58 +0530

    initialize pygame and redefine the values of height and width

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import time
 import pygame
 pygame.init()
 
+pygame.init()
 WIDTH, HEIGHT = 832, 614
 
+WIDTH, HEIGHT = 800, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Trainer")
 
