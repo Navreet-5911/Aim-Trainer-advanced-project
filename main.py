b43668d6f664add9ab4c8f1eb36332e625a460ff
Navreet Singh <naviibrar011@gmail.com>
2025-05-06 20:31:28 +0530

    Enhance target design with concentric circle layers

--- main.py
+++ main.py
@@ -44,8 +44,12 @@ class Target:
 
     def draw(self, win):
         pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size)
+        pygame.draw.circle(win, self.SECOND_COLOR,
+                           (self.x, self.y), self.size * 0.8)
         pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
         pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
+        pygame.draw.circle(win, self.SECOND_COLOR,
+                           (self.x, self.y), self.size * 0.4)
         pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
 
 
